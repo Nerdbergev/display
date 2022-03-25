ab22d1c48b5b538c1bc9e6d70456abee06a37125
Dominik Heidler <dheidler@suse.de>
2022-03-25 19:50:13 +0100

    On Exception show error on console as well as lauftext and reset after 30s

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import display
+import sys
+import time
+import machine
 
-display.setup()
 while True:
-	display.mainloop()
-
+	try:
+		display.setup()
+		while True:
+			display.mainloop()
+	except Exception as e:
+		print("Exception occured in main thread")
+		sys.print_exception(e)
+		display.lauftext = repr(e)
+		d = e
+		print("Resetting in 30s")
+		time.sleep(30)
+		machine.reset()
