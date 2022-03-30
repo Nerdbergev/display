898e75d1307cd6de9d3588be221fbd604ee7bfa5
Dominik Heidler <dheidler@suse.de>
2022-03-30 22:13:28 +0200

    Main loop shouldn't be needed due to machine.reset()

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import sys
 import time
 import machine
 
-while True:
-	try:
-		display.setup()
-		while True:
-			display.mainloop()
-	except Exception as e:
-		print("Exception occured in main thread")
-		sys.print_exception(e)
-		display.lauftext = repr(e)
-		d = e
-		print("Resetting in 30s")
-		time.sleep(30)
-		display.run_line2_thread = False
-		time.sleep(3)
-		machine.reset()
+try:
+	display.setup()
+	while True:
+		display.mainloop()
+except Exception as e:
+	print("Exception occured in main thread")
+	sys.print_exception(e)
+	display.lauftext = repr(e)
+	d = e
+	print("Resetting in 30s")
+	time.sleep(30)
+	display.run_line2_thread = False
+	time.sleep(3)
+	machine.reset()
