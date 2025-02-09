67760f0a644fa44e822f229faccbd5d1da107e55
Dominik Heidler <dheidler@suse.de>
2025-02-09 18:45:52 +0100

    Fix main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ try:
 except Exception as e:
 	print("Exception occured")
 	sys.print_exception(e)
-	display.lauftext = repr(e)
-	d = e
 	print("Resetting in 30s")
 	time.sleep(30)
 	machine.reset()
